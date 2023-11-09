9ed4ac84677a76125291d9250f0b6a3ec765495d
riosaputraduha <doehario@gmail.com>
2023-11-09 14:24:28 +0700

    add customer name input

--- PyMenuMate.py
+++ PyMenuMate.py
@@ -1,3 +1,7 @@
 print("-------------------- PROGRAM KASIR SEDERHANA PYMENUMATE --------------------")
 
+pembeli = input("Masukkan Nama Pelanggan : ")
+print('\n')
+print("Nama Pelanggan :", pembeli)
+
 
